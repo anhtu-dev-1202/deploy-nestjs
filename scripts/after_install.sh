d2ce4bedb18cea5a17255dc08338af21ad99e449
anhtu-dev-1202 <vungoctu.dev@gmail.com>
2023-03-24 17:55:39 +0700

    deploy

---
+++ scripts/after_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Stop the Node.js app
+pm2 stop deploy-nestjs
+
+# Move to the deployment directory
+cd /var/www/html/deploy-nestjs
+
+# Install dependencies
+npm install
+
+# Restart the Node.js app
+pm2 start deploy-nestjs
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install -y ruby
-cd /home/ubuntu
-curl -O https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo apt-get update
+sudo apt-get install -y ruby
+sudo apt-get install -y wget
+wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
 chmod +x ./install
-./install auto
+sudo ./install auto
