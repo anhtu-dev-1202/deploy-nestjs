b006aa39eb58d4007f68a72eeaaa350c059a7deb
anhtu-dev-1202 <vungoctu.dev@gmail.com>
2023-03-24 18:34:52 +0700

    deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt-get update
 sudo apt-get install -y ruby
 sudo apt-get install -y wget
-wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
+cd /home/ubuntu
+wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
+sudo service codedeploy-agent start
+
