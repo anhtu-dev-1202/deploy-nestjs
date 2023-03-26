15f11fd3c2a3f0536e93b72e14aa542a305b5b80
Lưu Phương <luphuowng@gmail.com>
2023-03-26 12:43:40 +0700

    Deploy nestjs

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y ruby
-sudo apt-get install -y wget
-cd /home/ubuntu
-wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-
