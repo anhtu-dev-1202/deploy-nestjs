d284a40312ae991f1058bc439e5d158db7e2d9be
Lưu Phương <luphuowng@gmail.com>
2023-03-26 13:26:37 +0700

    Deploy nestjs

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,10 +3,7 @@
 pm2 stop deploy-nestjs
 
 # Move to the deployment directory
-cd /var/www/html/deploy-nestjs
-
-# Install dependencies
-npm install
+cd /var/www/deploy-nestjs
 
 # Restart the Node.js app
 pm2 start deploy-nestjs
